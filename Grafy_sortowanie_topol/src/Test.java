74cd48e3ef56923565aee1d38e76fa3ed22e6235
Wojtek Lotko <wojteklotko3@gmail.com>
2022-04-07 10:32:56 +0200

    Added some simple graph for test purpose

--- Grafy_sortowanie_topol/src/Test.java
+++ Grafy_sortowanie_topol/src/Test.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Welcome!\n");
+        Graph_Topological_Sort g1 = new Graph_Topological_Sort();
+        g1.specifyNumberOfVertices(5);
+        g1.addEdge(0,3, 1);
+        g1.addEdge(0,1, 1);
+        g1.addEdge(1,2, 1);
+        g1.addEdge(3,1, 1);
+        g1.addEdge(4,2, 1);
+        g1.printGraph();
+        g1.sort_topo();
+
     }
 }
