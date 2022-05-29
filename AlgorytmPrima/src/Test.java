a4f8361dfdf43e06b415a7ea0eb7922f0c3440c6
Logger421 <wojteklotko3@gmail.com>
2022-05-30 00:00:14 +0200

    Added method to print visited list to terminal

--- AlgorytmPrima/src/Test.java
+++ AlgorytmPrima/src/Test.java
@@ -1,8 +1,15 @@
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         MST prims = new MST();
         prims.readFile();
         int weights_sum = prims.primsAlgorithm();
         System.out.println("Suma wag krawędzi: " + weights_sum);
+        showVisitedList(prims.visited, 4);
+    }
+    public static void showVisitedList(List<Integer> visited, int n) {
+        System.out.println("Kolejność dodawania wierzchołków do listy zaczynająć od indeksu " + n + " ");
+        System.out.print(visited);
     }
 }
